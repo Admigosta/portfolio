6ff1bb9dc156f5d7cc8867c656553173b5c5dfc3
Adão Francisco <103819900+Admigosta@users.noreply.github.com>
2026-06-06 02:41:15 +0100

    Apresentação de portfolio inicial
    Nesta etapa a os formularios de mensagem não estão funcionais

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+
+// Correção para o __dirname funcionar em ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+function figmaAssetResolver() {
+  return {
+    name: 'figma-asset-resolver',
+    // CORREÇÃO AQUI: Adicionamos ": string" para o TypeScript ficar feliz
+    resolveId(id: string) { 
+      if (id.startsWith('figma:asset/')) {
+        const filename = id.replace('figma:asset/', '')
+        return path.resolve(__dirname, 'src/assets', filename)
+      }
+    },
+  }
+}
+
+export default defineConfig({
+  base: '/portfolio/', 
+  plugins: [
+    figmaAssetResolver(),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+})
